84b9549e4c431ab62fd9d40c3cd63c9bc85efae8
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-01-18 11:54:16 -0600

    finished task 2

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+   const champ = data.filter(team => team.Stage === 'Final')
+   return champ
 }
 
 
